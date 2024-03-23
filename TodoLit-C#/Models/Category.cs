3bfb062fb388859dadbbcc076d5dde480b6d981e
Rupam0710 <rupampal48@gmail.com>
2024-03-24 02:23:14 +0530

    1st com - Full TodoList Working code in C#

---
+++ TodoLit-C#/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoLit_C_.Models
+{
+    public class Category
+    {
+        public string categoryId { get; set; } = string.Empty;
+
+        public string Name { get; set; } = string.Empty ;
+    }
+}
---
+++ TodoLit-C#/Models/ToDoContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoLit_C_.Models
+{
+    public class ToDoContext : DbContext
+    {
+        public ToDoContext(DbContextOptions<ToDoContext> options) : base(options) { }
+
+        public DbSet<Todo> Todos { get; set; } = null!;
+
+        public DbSet<Category> Categories { get; set; } = null!;
+
+        public DbSet<Status> Statuses { get; set; } = null!;
+
+
+        //seed data
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { categoryId = "work", Name = "Work" },
+                new Category { categoryId = "home", Name = "Home" },
+                new Category { categoryId = "ex", Name = "Exercise" },
+                new Category { categoryId = "shop", Name = "Shopping" },
+                new Category { categoryId = "call", Name = "Contact" }
+                );
+
+            modelBuilder.Entity<Status>().HasData(
+              new Status { StatusId = "open", Name = "Open" },
+              new Status { StatusId = "closed", Name = "Completed" }
+              
+              );
+        }
+
+
+    }
+}
